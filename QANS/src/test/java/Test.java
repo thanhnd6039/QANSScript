e18c558cbd5dbd814cfcb65e55b6269f4e51a8fd
thanhnd6039 <thanhvirtium@gmail.com>
2023-09-14 17:08:23 +0700

    Tracked Opp Dashboard

--- QANS/src/test/java/Test.java
+++ QANS/src/test/java/Test.java
@@ -1,27 +1,12 @@
-import Helpers.DataProvider.ExcelReader;
-import Helpers.Manager.FileReaderManager;
 import Pages.Reports.RSSaleGapAccountAssignmentPage;
-import org.apache.commons.collections4.list.SetUniqueList;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import Pages.Reports.RSTrackedOppDashboardPage;
 
 
 public class Test {
     public static void main(String[] args) {
         String filePath = "C:\\CucumberFramework\\Downloads\\Output.xlsx";
-        RSSaleGapAccountAssignmentPage rsSaleGapAccountAssignmentPage = new RSSaleGapAccountAssignmentPage();
-        rsSaleGapAccountAssignmentPage.getAllOEMGroupByMainSaleRep();
+        RSTrackedOppDashboardPage rsTrackedOppDashboardPage = new RSTrackedOppDashboardPage();
+        rsTrackedOppDashboardPage.getDataFromTrackedOppDashboard();
         System.out.println("Done");
     }
 }
